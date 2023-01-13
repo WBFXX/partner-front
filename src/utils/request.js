917014b0e1616db588e7644be52474adc68b9f95
帆 <1157712386@qq.com>
2023-01-13 20:23:56 +0800

    视频1完结

--- src/utils/request.js
+++ src/utils/request.js
@@ -11,6 +11,8 @@ const request = axios.create({
 
 // request 拦截器
 // 可以自请求发送前对请求做一些处理
+
+
 // 比如统一加token，对请求参数统一加密
 request.interceptors.request.use(config => {
     config.headers['Content-Type'] = 'application/json;charset=utf-8';
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,19 +1,21 @@
 <template>
-  <div style="height: 100vh;overflow: hidden; position: relative; background-image: linear-gradient(135deg, #f5f7fa 0%, #c3cfe2 100%);">
-    <div style="width: 300px; border-radius: 10px; margin: 0 auto; box-shadow: 0 0 8px -2px rgba(0,0,0,.5);
-    background-image: linear-gradient(120deg, #a1c4fd 0%, #c2e9fb 100%); padding: 20px;
-      position: absolute; top: 50%; left: 50%; transform: translateX(-50%) translateY(-50%);">
-      <el-form>
-        <h2 style="text-align: center;">后台登录</h2>
-        <el-form-item prop="account">
-          <el-input v-model="form.account"  placeholder="请输入账号"></el-input>
+  <div
+      style="height: 100vh;overflow: hidden; position: relative; background-image: linear-gradient(135deg, #f5f7fa 0%, #c3cfe2 100%);">
+    <div class="form-box">
+      <el-form ref="ruleFormRef" :model="form" :rules="rules" status-icon>
+        <h2 style="text-align: center;">登录</h2>
+        <el-form-item prop="username">
+          <el-input v-model="form.username" placeholder="请输入账号" :prefix-icon="User"></el-input>
         </el-form-item>
-        <el-form-item prop="password">
-          <el-input v-model="form.password"  placeholder="请输入密码"
-                    autocomplete="new-password"></el-input>
+        <el-form-item prop="password" >
+          <el-input v-model="form.password" placeholder="请输入密码" type="password"
+                    autocomplete="new-password" :prefix-icon="Lock"></el-input>
         </el-form-item>
-        <div style="margin-bottom: 0.83em">
+        <div style="margin-bottom: 0.83em" @click="login">
           <el-button style="width: 100%" type="primary">登录</el-button>
+        </div>
+        <div style="text-align: right">
+          <el-button type="primary" link @click="router.push('/register')" >没有账号？请注册</el-button>
         </div>
       </el-form>
     </div>
@@ -21,10 +23,50 @@
 </template>
 
 <script setup>
-import {  reactive  } from "vue"
+import {reactive, ref} from "vue"
+//import {FormInstance, FormRules} from 'element-plus'
+import {User,Lock} from '@element-plus/icons-vue'
+import router from "@/router";
+import request from "@/utils/request";
+import {ElMessage} from "element-plus";
+const ruleFormRef = ref()
+const rules = reactive({
+  username: [
+    {required: true, message: '请输入账号', trigger: 'blur'},
+  ],
+  password: [
+    {required: true, message: '请输入正确密码', trigger: 'blur'},
+  ],
+})
 const form = reactive({})
+
+const login = () =>{
+  console.log(ruleFormRef)
+  ruleFormRef.value.validate(valid => {
+    //当valid== true 就调用登录
+    request.post("/login",form).then(res => {
+      console.log(res)
+      if(res.code === '200'){
+        ElMessage.success('登录成功')
+        router.push("/")
+      }else ElMessage.error(res.msg)
+    })
+  })
+}
+
 </script>
 
 <style scoped>
-
+.form-box {
+  width: 300px;
+  border-radius: 10px;
+  margin: 0 auto;
+  box-shadow: 0 0 8px -2px rgba(0, 0, 0, .5);
+  background-image: linear-gradient(120deg, #a1c4fd 0%, #c2e9fb 100%);
+  padding: 20px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translateX(-50%) translateY(-50%);
+}
 </style>
